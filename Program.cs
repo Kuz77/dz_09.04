509fe241cb247d98f0e5de46aa00edfe2b6e8183
Ilya <kuzovlev92@mail.ru>
2023-04-11 10:52:52 +0300

    для пуша

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+        
+Console.WriteLine("Чётные числа от 1 до {0}:", n);
+for (int i = 2; i <= n; i += 2)
+{
+    Console.WriteLine(i);
+}
